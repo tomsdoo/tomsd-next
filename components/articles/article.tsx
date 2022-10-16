3a90ba37ba298357ab6c728e86a8514584ec5a6f
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-17 05:35:35 +0900

    Article component is defined out of page (#3)

---
+++ components/articles/article.tsx
@@ -0,0 +1,29 @@
+import { gql, useQuery } from "@apollo/client";
+
+const QUERY_ARTICLE = gql(`
+  query article($id: Int!) {
+    article(id: $id) {
+      id
+      title
+      content
+    }
+  }
+  `);
+
+const Article = ({ articleId }) => {
+  const { loading, error, data } = useQuery(QUERY_ARTICLE, {
+    variables: { id: articleId }
+  });
+  if(loading){return <div>loading</div>;}
+  if(error){return <div>error</div>;}
+
+  return (
+    <div>
+      <div>{data.article.id}</div>
+      <div>{data.article.title}</div>
+      <div>{data.article.content}</div>
+    </div>
+  );
+}
+
+export default Article;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,18 @@
 import Head from "next/head";
-import { gql, useQuery } from "@apollo/client";
-import MyApolloProvider from "../components/providers/apollo";
-
-const QUERY_ARTICLE = gql(`
-  query article($id: Int!) {
-    article(id: $id) {
-      id
-      title
-      content
-    }
-  }
-`);
-
-const Article = ({ aid }) => {
-  const { loading, error, data } = useQuery(QUERY_ARTICLE, {
-    variables: { id: aid }
-  });
-  if(loading){console.log(process.env.ORIGIN);return <div>loading</div>;}
-  if(error){return <div>error</div>;}
-
-  return (
-    <div>
-      <div>{data.article.id}</div>
-      <div>{data.article.title}</div>
-      <div>{data.article.content}</div>
-    </div>
-  );
-};
+import ApolloProviderForArticles from "../components/providers/apollo";
+import Article from "../components/articles/article";
 
 export default function Page(){
   const message = "hello world";
   return (
-    <MyApolloProvider>
+    <ApolloProviderForArticles>
       <Head>
         <title>test</title>
       </Head>
       <div>
         {message}
       </div>
-      <Article aid={2} />
-    </MyApolloProvider>
+      <Article articleId={1} />
+    </ApolloProviderForArticles>
   );
 }
