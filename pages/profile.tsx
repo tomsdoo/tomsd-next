6f0a7e3a5ed1e2f65590adc4a51f2d3c14b8236f
tom <toms@live.jp>
2022-10-17 18:10:56 +0900

    header and main is added on profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,14 +1,22 @@
 import DynamicHead from "@/components/heads/head";
-import headerStyles from "@/styles/pages/profile.module.css";
+import pageStyles from "@/styles/pages/profile.module.css";
 
 export default function Page(){
   return (
     <>
       <DynamicHead />
       <div>
-        <header className={headerStyles.header}>
-          wow
-        </header>
+        <div className={pageStyles.cover}></div>
+        <div className={pageStyles.screen}>
+          <header className={pageStyles.header}>
+            wow
+          </header>
+          <main className={pageStyles.main}>
+            <article className={pageStyles.article}>
+              content
+            </article>
+          </main>
+        </div>
       </div>
     </>
   );
