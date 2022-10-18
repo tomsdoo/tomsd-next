28f6d68a8a875502c12249314568c275e010afa2
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-18 18:37:33 +0900

    names styles are added (#12)
    * Profile is added in components
    
    * named styles are added
    
    * styles are coordinated

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,48 +1,17 @@
 import DynamicHead from "@/components/heads/head";
 import Layout from "@/layouts/covered";
 import pageStyles from "@/styles/pages/profile.module.css";
+import frostedStyles from "@/styles/named/frosted.module.css";
+import shadowStyles from "@/styles/named/shadow.module.css";
 import ApolloProvider from "@/components/providers/apollo";
-
-import { gql, useQuery } from "@apollo/client";
-const QUERY_PROFILE = gql(`
-  query profile {
-    profile {
-      name
-      photo
-      title
-      description
-      location
-      certificates {
-        title
-        when
-      }
-      favorites
-      links {
-        name
-        image
-        url
-        shortName
-      }
-    }
-  }
-  `);
-
-function Profile(){
-  const { loading, error, data } = useQuery(QUERY_PROFILE);
-  if(loading){return <div>loading</div>;}
-  if(error){return <div>error</div>;}
-
-  return (
-    <div>{data.profile.name}</div>
-  );
-}
+import Profile from "@/components/articles/profile";
 
 export default function Page(){
   return (
     <ApolloProvider>
       <DynamicHead />
       <Layout>
-        <article className={pageStyles.article}>
+        <article className={`${pageStyles.article} ${frostedStyles.frosted} ${shadowStyles.shadow_2_8_787878}`}>
           <Profile />
         </article>
       </Layout>
