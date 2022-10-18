ffc3574a43bc8ee7690fc1bfee563f932470b59f
tom <toms@live.jp>
2022-10-18 18:24:12 +0900

    named styles are added

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,13 +1,15 @@
 import DynamicHead from "@/components/heads/head";
 import Layout from "@/layouts/covered";
 import pageStyles from "@/styles/pages/profile.module.css";
+import frostedStyles from "@/styles/named/frosted.module.css";
+import shadowStyles from "@/styles/named/shadow.module.css";
 
 export default function Page(){
   return (
     <>
       <DynamicHead />
       <Layout>
-        <article className={pageStyles.article}>
+        <article className={`${pageStyles.article} ${frostedStyles.frosted} ${shadowStyles.shadow_2_8_787878}`}>
           skills
         </article>
       </Layout>
