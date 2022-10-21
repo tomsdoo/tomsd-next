63400015be42ce14a78f74ac6d8c81ff78f1429f
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-21 19:26:16 +0900

    artifact components are added (#23)
    * Artifact and Artifacts are added
    
    * Artifacts component is moved

--- pages/links.tsx
+++ pages/links.tsx
@@ -2,38 +2,16 @@ import DynamicHead from "@/components/heads/head";
 import Layout from "@/layouts/covered";
 import pageStyles from "@/styles/pages/skills.module.css";
 import ApolloProvider from "@/components/providers/apollo";
-
-import { gql, useQuery } from "@apollo/client";
-const QUERY_ARTIFACTS = gql(`
-  query artifacts {
-    artifacts {
-      title
-      link
-      description
-      image
-      orderScore
-    }
-  }
-  `);
-
-function Artifacts(){
-  const { loading, error, data } = useQuery(QUERY_ARTIFACTS);
-  if(loading){return <div>loading</div>;}
-  if(error){return <div>error</div>;}
-
-  return (
-    <ul>
-      {data.artifacts.map(artifact => <li>{artifact.title}</li>)}
-    </ul>
-  );
-}
+import Artifacts from "@/components/articles/artifacts";
 
 export default function Page(){
   return (
     <ApolloProvider>
       <DynamicHead />
       <Layout loaded={true}>
-        <Artifacts />
+        <article className={pageStyles.article}>
+          <Artifacts />
+        </article>
       </Layout>
     </ApolloProvider>
   );
