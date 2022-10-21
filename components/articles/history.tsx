a32cf7ea4d9211fdd50c296e6cfd5567775cb8a4
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-21 14:13:38 +0900

    date-fns is used on history page (#19)
    * date-fns is installed
    
    * history layout is changed
    
    * frosted style is coordinated

--- components/articles/history.tsx
+++ components/articles/history.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import historyStyles from "@/styles/components/articles/history.module.css";
+import { format, differenceInYears, differenceInMonths } from "date-fns";
 
 const QUERY_HISTORIES = gql(`
   query histories {
@@ -14,6 +15,27 @@ const QUERY_HISTORIES = gql(`
   }
   `);
 
+
+function getTerm({ start, end }){
+  const localStart = new Date(start);
+  const localEnd = (end && new Date(end)) ?? new Date();
+  const totalMonths = differenceInMonths(localEnd, localStart);
+  const term = {
+    years: Math.floor(totalMonths / 12),
+    months: totalMonths % 12
+  };
+  return {
+    ...term,
+    text: [
+      term.years > 0 ? `${term.years}年` : "",
+      `${term.months}月`
+    ].join(" ")
+  };
+}
+function formatYM(date?: string){
+  return format((date && new Date(date)) ?? new Date(), "yyyy.MM");
+}
+
 function Badges({ badges, ...props }){
   return (
     <ul {...props}>
@@ -23,11 +45,14 @@ function Badges({ badges, ...props }){
 }
 
 function HistoryItem({history: { companyDescription, title, start, end, badges, description }}){
+  const term = getTerm({ start, end });
   return (
     <>
-      <span className={historyStyles.term}>{start} - {end}</span>
-      <div className={historyStyles.company}>{companyDescription}</div>
-      <div className={historyStyles.title}>{title}</div>
+      <span className={historyStyles.term}><span className={historyStyles.span}>{formatYM(start)} - {formatYM(end)}</span>({term.text})</span>
+      <div className={historyStyles.companyAndTitle}>
+        <div className={historyStyles.company}>{companyDescription}</div>
+        <div className={historyStyles.title}>{title}</div>
+      </div>
       <Badges className={historyStyles.badges} badges={badges} />
       <div className={historyStyles.description}>{description}</div>
     </>
@@ -39,8 +64,10 @@ export default function History(){
   if(loading){return;}
   if(error){return;}
   return (
-    <ol className={historyStyles.list}>
-      {data.histories.map(history => <li className={historyStyles.item}><HistoryItem history={history} /></li>)}
-    </ol>
+    <div className={historyStyles.area}>
+      <ol className={historyStyles.list}>
+        {data.histories.map(history => <li className={historyStyles.item}><HistoryItem history={history} /></li>)}
+      </ol>
+    </div>
   );
 }
