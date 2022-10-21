9dd88689730cc8d49064ef1d076f61e133fd72c2
tom <toms@live.jp>
2022-10-21 19:23:52 +0900

    Artifact and Artifacts are added

---
+++ components/articles/artifacts.tsx
@@ -0,0 +1,29 @@
+import { gql, useQuery } from "@apollo/client";
+import artifactsStyles from "@/styles/components/articles/artifacts.module.css";
+import Artifact from "@/components/articles/artifact";
+
+const QUERY_ARTIFACTS = gql(`
+  query artifacts {
+    artifacts {
+      title
+      link
+      description
+      image
+      orderScore
+    }
+  }
+  `);
+
+export default function Artifacts(){
+  const { loading, error, data } = useQuery(QUERY_ARTIFACTS);
+  if(loading){return <div>loading</div>;}
+  if(error){return <div>error</div>;}
+
+  const artifacts = [...data.artifacts].sort((a,b) => b.orderScore - a.orderScore);
+
+  return (
+    <ul className={artifactsStyles.list}>
+      {artifacts.map(artifact => <Artifact artifact={artifact} />)}
+    </ul>
+  );
+}
