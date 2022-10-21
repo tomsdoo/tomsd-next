e243f94a3a13954d6ecca4f2ee7af95fbb82025a
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-21 17:21:45 +0900

    skill component style is changed (#20)

--- components/articles/skill.tsx
+++ components/articles/skill.tsx
@@ -3,10 +3,15 @@ import shadowStyles from "@/styles/named/shadow.module.css";
 import skillStyles from "@/styles/components/articles/skill.module.css";
 
 export default function Skill({skill}){
+  const maxYears = 5;
+  const years = skill.years > maxYears
+    ? `${maxYears}+`
+    : `${skill.years}`;
+
   return (
     <div className={`${skillStyles.item} ${frostedStyles.frosted} ${shadowStyles.shadow_2_8_787878}`}>
       <span className={skillStyles.title}>{skill.title}</span>
-      <span className={skillStyles.years}>{skill.years}</span>
+      <span className={skillStyles.years}>{years} yrs</span>
       <div className={skillStyles.description}>{skill.description}</div>
     </div>
   );
