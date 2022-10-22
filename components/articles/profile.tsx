1273c2e2321246dfaf312c03f4592c0409afe518
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-23 05:40:31 +0900

    profile layout is changed (#29)

--- components/articles/profile.tsx
+++ components/articles/profile.tsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import styles from "@/styles/components/articles/profile.module.css";
 
 const QUERY_PROFILE = gql(`
   query profile {
@@ -29,9 +30,32 @@ export default function Profile(){
   if(error){return <div>error</div>;}
 
   return (
-    <>
-      <div>{data.profile.name}</div>
-      <div>{data.profile.title}</div>
-    </>
+    <div className={styles.profile}>
+      <div className={styles.name}>{data.profile.name}</div>
+      <div className={styles.title}>{data.profile.title}</div>
+      <div className={styles.description}>{data.profile.description}</div>
+      <div className={`${styles.location} ${styles.iconAndContent}`}>
+        <span className={`${styles.icon} material-icons`}>place</span>
+        <div className={styles.content}>{data.profile.location}</div>
+      </div>
+      <div className={`${styles.certificates} ${styles.iconAndContent}`}>
+        <span className={`${styles.icon} material-icons`}>verified</span>
+        <ul>
+          {data.profile.certificates.map(certificate => <li>{certificate.title}</li>)}
+        </ul>
+      </div>
+      <div className={`${styles.favorites} ${styles.iconAndContent}`}>
+        <span className={`${styles.icon} material-icons`}>favorite</span>
+        <ul>
+          {data.profile.favorites.map(favorite => <li>{favorite}</li>)}
+        </ul>
+      </div>
+      <div className={`${styles.links} ${styles.iconAndContent}`}>
+        <span className={`${styles.icon} material-icons`}>link</span>
+        <ul>
+          {data.profile.links.map(link => <li><a className={styles.link} href={link.url} target="_blank"><img className={styles.image} src={link.image} /><span className={styles.url}>{link.url}</span></a></li>)}
+        </ul>
+      </div>
+    </div>
   );
 }
