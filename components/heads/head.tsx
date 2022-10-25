0d7b036a770191d6177d584150b33d531a79f354
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-25 19:17:11 +0900

    routes is applied in DynamicHead (#43)

--- components/heads/head.tsx
+++ components/heads/head.tsx
@@ -1,34 +1,18 @@
 import { useRouter } from "next/router";
 import Head from "next/head";
+import { routes } from "@/routes/index";
 
 export default function DynamicHead(){
   const siteName = "tomsd";
 
   const headCandidates = [
-    {
-      pathRegexp: /^\/profile\/?/,
-      title: `profile | ${siteName}`,
-      description: "tomsd designs and develops Web and the operations."
-    },
-    {
-      pathRegexp: /^\/skills\/?/,
-      title: `skills | ${siteName}`,
-      description: "tomsd has the experiences of the Web and the system development."
-    },
-    {
-      pathRegexp: /^\/history\/?/,
-      title: `history | ${siteName}`,
-      description: "History of tomsd will tell you what he has deed."
-    },
-    {
-      pathRegexp: /^\/links\/?/,
-      title: `links | ${siteName}`,
-      description: "Links about tomsd indicate that his artifacts are there."
-    },
+    ...routes,
     {
       pathRegexp: /.*/,
-      title: siteName,
-      description: "tomsd's web site."
+      meta: {
+        title: siteName,
+        description: "tomsd's web site."
+      }
     }
   ];
 
@@ -39,8 +23,8 @@ export default function DynamicHead(){
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <meta charSet="UTF-8" />
-      <meta name="description" content={displayingHead.description} />
-      <title>{displayingHead.title}</title>
+      <meta name="description" content={displayingHead.meta.description} />
+      <title>{displayingHead.meta.title}</title>
     </Head>
   );
 }
