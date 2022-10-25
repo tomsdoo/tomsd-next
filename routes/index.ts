c98dfc3440ff91983ad1bcf261c1f9aef9b79664
tom <toms@live.jp>
2022-10-25 19:13:26 +0900

    routes is applied in DynamicHead

--- routes/index.ts
+++ routes/index.ts
@@ -1,26 +1,48 @@
+const siteName = "tomsd";
+
 export const routes = [
   {
     href: "/profile",
+    pathRegexp: /^\/profile\/?/,
     headerLink: {
       title: "profile"
+    },
+    meta: {
+      title: `profile | ${siteName}`,
+      description: "tomsd designs and develops Web and the operations."
     }
   },
   {
     href: "/skills",
+    pathRegexp: /^\/skills\/?/,
     headerLink: {
       title: "skills"
+    },
+    meta: {
+      title: `skills | ${siteName}`,
+      description: "tomsd has the experiences of the Web and the system development."
     }
   },
   {
       href: "/history",
+      pathRegexp: /^\/history\/?/,
       headerLink: {
         title: "history"
+      },
+      meta: {
+        title: `history | ${siteName}`,
+        description: "History of tomsd will tell you what he has deed."
       }
   },
   {
     href: "/links",
+    pathRegexp: /^\/links\/?/,
     headerLink: {
       title: "links"
+    },
+    meta: {
+      title: `links | ${siteName}`,
+      description: "Links about tomsd indicate that his artifacts are there."
     }
   }
 ];
