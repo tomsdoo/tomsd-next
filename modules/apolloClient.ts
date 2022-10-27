157a0c2253b60b80ee77a05df1b9938b9d54be4a
tom <toms@live.jp>
2022-10-27 18:59:25 +0900

    format:modules

--- modules/apolloClient.ts
+++ modules/apolloClient.ts
@@ -3,15 +3,17 @@ import { setContext } from "@apollo/client/link/context";
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: setContext((_, { headers}) => {
+  link: setContext((_, { headers }) => {
     const requestedWith = "tomsd-client";
     return {
       headers: {
         ...headers,
-        "x-requested-with": requestedWith
-      }
+        "x-requested-with": requestedWith,
+      },
     };
-  }).concat(new HttpLink({
-    uri: [process.env.ORIGIN, "/api/gql"].join("")
-  }))
+  }).concat(
+    new HttpLink({
+      uri: [process.env.ORIGIN, "/api/gql"].join(""),
+    })
+  ),
 });
