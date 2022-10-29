95fc0ad4f0876c5c2c4e95c60aaf8327a3e5f66b
tom <toms@live.jp>
2022-10-29 10:17:03 +0900

    skills are sorted in Skills component

--- components/articles/skills.tsx
+++ components/articles/skills.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from "react";
 import { gql, useQuery } from "@apollo/client";
 import skillsStyles from "@/styles/components/articles/skills.module.css";
 import Skill from "@/components/articles/skill";
+import { extendSkill } from "@/modules/misc/operationsForSkill";
 
 const QUERY_SKILLS = gql(`
   query skills {
@@ -23,9 +24,14 @@ export default function Skills(): ReactElement {
     return <div>error</div>;
   }
 
+  const skills = data.skills
+    .slice()
+    .map(extendSkill)
+    .sort((a, b) => b.score - a.score);
+
   return (
     <ul className={skillsStyles.list}>
-      {data.skills.map((skill) => (
+      {skills.map((skill) => (
         <li key={skill.title}>
           <Skill skill={skill} />
         </li>
