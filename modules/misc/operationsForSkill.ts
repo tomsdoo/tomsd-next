290dcba2d456984a947399ddddef6c0fc58e898f
tom <toms@live.jp>
2022-10-29 10:15:52 +0900

    operations for skill module is added

---
+++ modules/misc/operationsForSkill.ts
@@ -0,0 +1,27 @@
+interface Skill {
+  web: boolean;
+  years: number;
+  scoreWeb?: number;
+  score?: number;
+}
+
+export function addScoreWeb(skill: Skill){
+  return {
+    ...skill,
+    scoreWeb: skill.web ? 100 : 1
+  };
+}
+
+export function addScore(skill: Skill){
+  return {
+    ...skill,
+    score: skill.scoreWeb + skill.years
+  };
+}
+
+export function extendSkill(skill: Skill){
+  return [
+    addScoreWeb,
+    addScore
+  ].reduce((currentSkill, fn) => fn(currentSkill), {...skill})
+}
