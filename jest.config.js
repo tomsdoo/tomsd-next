f1399aef294d9b797c876587843ef0ea8f165b6c
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-10-31 17:03:26 +0900

    test for Artifact component (#70)
    * jest config is changed
    
    * test for Artifact component

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,10 @@ const path = require("path");
 module.exports = {
   moduleNameMapper: {
     "^@/apollo/(.*)$": "<rootDir>/apollo/$1",
+    "^@/components/(.*)$": "<rootDir>/components/$1",
     "^@/modules/(.*)$": "<rootDir>/modules/$1",
-    "^@/routes/(.*)$": "<rootDir>/routes/$1"
+    "^@/routes/(.*)$": "<rootDir>/routes/$1",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
   },
   transform: {
     "^.+\\.tsx?$": path.join(__dirname, "jest.babel.config.js")
