c8e64eef54f87e5ae4f38e7336509e3bb1cc97d3
tom <toms@live.jp>
2022-10-31 14:31:58 +0900

    jest config is changed

--- jest.babel.config.js
+++ jest.babel.config.js
@@ -3,6 +3,7 @@ const babelJest = require("babel-jest");
 const babelOptions = {
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
+    ['@babel/preset-react', { runtime: "automatic" }],
     '@babel/preset-typescript',
   ]
 };
