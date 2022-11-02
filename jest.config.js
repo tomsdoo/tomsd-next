04c086551f154189bc246e7f1702e7a1f2c63fed
tom <toms@live.jp>
2022-11-02 17:51:56 +0900

    test for covoered layout

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   moduleNameMapper: {
     "^@/apollo/(.*)$": "<rootDir>/apollo/$1",
     "^@/components/(.*)$": "<rootDir>/components/$1",
+    "@/layouts/(.*)$": "<rootDir>/layouts/$1",
     "^@/modules/(.*)$": "<rootDir>/modules/$1",
     "^@/routes/(.*)$": "<rootDir>/routes/$1",
     "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
