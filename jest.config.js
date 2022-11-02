d428bb7ee591730202138fbb6415524d3590c3d5
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-11-03 06:03:58 +0900

    test for 404 page (#80)

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,9 @@ module.exports = {
   moduleNameMapper: {
     "^@/apollo/(.*)$": "<rootDir>/apollo/$1",
     "^@/components/(.*)$": "<rootDir>/components/$1",
-    "@/layouts/(.*)$": "<rootDir>/layouts/$1",
+    "^@/layouts/(.*)$": "<rootDir>/layouts/$1",
     "^@/modules/(.*)$": "<rootDir>/modules/$1",
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
     "^@/routes/(.*)$": "<rootDir>/routes/$1",
     "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.js"
   },
