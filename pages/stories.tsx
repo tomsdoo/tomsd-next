53a956f510a8adb8856d22c62037cbe297ab0034
tom <toms@live.jp>
2022-11-04 17:02:05 +0900

    /stories is initially added

---
+++ pages/stories.tsx
@@ -0,0 +1,67 @@
+import { ReactElement } from "react";
+import DynamicHead from "@/components/heads/head";
+import Layout from "@/layouts/covered";
+import ApolloProvider from "@/components/providers/apollo";
+
+import { gql, useQuery } from "@apollo/client";
+
+const QUERY_STORIES = gql(`
+  query stories {
+    stories {
+      title
+      description
+      issue
+      solution
+      badges
+    }
+  }
+  `);
+
+function Story({ story }): ReactElement {
+  return (
+    <div>
+      <div>{story.title}</div>
+      <div>{story.description}</div>
+      <div>{story.issue}</div>
+      <div>{story.solution}</div>
+      <div>{JSON.stringify(story.badges)}</div>
+    </div>
+  );
+}
+
+function Stories(): ReactElement {
+  const { loading, error, data } = useQuery(QUERY_STORIES);
+  if (loading) {
+    return;
+  }
+  if (error) {
+    return;
+  }
+  console.log(data);
+  const stories = data.stories;
+
+  console.log(stories);
+
+  return (
+    <ul>
+      {stories.map((story, index) => (
+        <li key={index}>
+          <Story story={story} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default function Page(): ReactElement {
+  return (
+    <ApolloProvider>
+      <DynamicHead />
+      <Layout loaded={true}>
+        <article>
+          <Stories />
+        </article>
+      </Layout>
+    </ApolloProvider>
+  );
+}
