7966aa22f14d89aafcc953d6aef6b34718354997
tom <toms@live.jp>
2022-11-10 14:04:58 +0900

    stories are added

---
+++ stories/components/articles/artifact.stories.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Artifact from "@/components/articles/artifact";
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+  title: "components/articles/Artifact",
+  component: Artifact,
+} as ComponentMeta<typeof Artifact>;
+
+const Template: ComponentStory<typeof Artifact> = (args) => (
+  <Artifact {...args} />
+);
+
+export const Default: ComponentStory<typeof Artifact> = Template.bind({});
+Default.args = {
+  artifact: {
+    link: "https://www.google.com",
+    title: "title",
+    description: "description",
+    image: "https://picsum.photos/200",
+  },
+};
+Default.storyName = "defalut";
