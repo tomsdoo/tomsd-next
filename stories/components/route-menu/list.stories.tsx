4cc1d5ba0d8f22b598d13195dad2266e7fd34f95
tomsdoo <toms@live.jp>
2025-07-24 08:51:50 +0900

    RouteMenuList component

---
+++ stories/components/route-menu/list.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { StoryObj, Meta } from "@storybook/nextjs";
+
+import RouteMenuList from "@/components/route-menu/list";
+
+export default {
+  title: "components/route-menu/RouteMenuList",
+  component: RouteMenuList,
+} as Meta<typeof RouteMenuList>;
+
+export const Default: StoryObj<typeof RouteMenuList> = {
+  args: {
+    routes: [0, 1, 2].map((i) => ({
+      href: `dummyUrl${i}`,
+      pathRegexp: new RegExp(`dummy${i}`),
+      headerLink: {
+        title: `title${i}`,
+      },
+      meta: {
+        title: `dummyMetaTitle${i}`,
+        description: `dummyMetaDescription${i}`,
+      },
+    })),
+  },
+  storyName: "default",
+  render: (args) => (
+    <div
+      style={{
+        color: "#eeeeee",
+        background: "#232323",
+        width: "min(2-rem, calc(100vw - 2erm))",
+      }}
+    >
+      <RouteMenuList {...args}></RouteMenuList>
+    </div>
+  ),
+};
