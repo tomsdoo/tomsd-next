784b6383dfa2f627b3965e2b9fa2772ae984b2bb
tomsdoo <toms@live.jp>
2025-07-26 08:17:32 +0900

    pages/profile.tsx -> app/profile/page.tsx

--- __test__/pages/profile.test.tsx
+++ __test__/pages/profile.test.tsx
@@ -5,14 +5,14 @@ import { describe, it, expect, jest } from "@jest/globals";
 import "@testing-library/jest-dom";
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Page from "@/pages/profile";
+import Page from "@/app/profile/page";
 
 jest.mock(
   "@/components/heads/head",
   () =>
     function MockedHead() {
       return <div data-testid="mocked-head">mocked head</div>;
-    }
+    },
 );
 
 jest.mock(
@@ -20,7 +20,7 @@ jest.mock(
   () =>
     function MockedLayout({ children }) {
       return <div data-testid="mocked-layout">{children}</div>;
-    }
+    },
 );
 
 jest.mock(
@@ -28,7 +28,7 @@ jest.mock(
   () =>
     function MockedProvider({ children }) {
       return <div data-testid="mocked-provider">{children}</div>;
-    }
+    },
 );
 
 jest.mock(
@@ -36,7 +36,7 @@ jest.mock(
   () =>
     function MockedProfile() {
       return <div data-testid="mocked-profile">mocked profile</div>;
-    }
+    },
 );
 
 describe("profile page", () => {
