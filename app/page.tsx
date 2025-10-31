1ca4ba1a1df0100639f129f9ba25a5c640322a43
tomsdoo <toms@live.jp>
2025-10-31 19:08:58 +0900

    rm next.config.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
-import { ReactElement } from "react";
+import packageJson from "@@/package.json" with { type: "json" };
+import type { ReactElement } from "react";
 import DynamicHead from "@/components/heads/head";
+import PhotoList from "@/components/home/images/photo-list";
+import RouteMenuForm from "@/components/home/route-menu/form";
 import { routes } from "@/routes/index";
 import styles from "@/styles/pages/index.module.css";
-import getConfig from "next/config";
-import RouteMenuForm from "@/components/home/route-menu/form";
-import PhotoList from "@/components/home/images/photo-list";
-
-const { publicRuntimeConfig } = getConfig();
 
 export default function Page(): ReactElement {
-  console.log(`version: ${publicRuntimeConfig.version as string}`);
-  return (
-    <>
-      <DynamicHead />
-      <div className={styles.screen}>
-        <PhotoList className={styles.photoList}></PhotoList>
-        <RouteMenuForm className={styles.form} routes={routes}></RouteMenuForm>
-      </div>
-    </>
-  );
+	console.log(`version: ${packageJson.version as string}`);
+	return (
+		<>
+			<DynamicHead />
+			<div className={styles.screen}>
+				<PhotoList className={styles.photoList}></PhotoList>
+				<RouteMenuForm className={styles.form} routes={routes}></RouteMenuForm>
+			</div>
+		</>
+	);
 }
